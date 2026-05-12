f502d592c28f8a064e8e8cc4993aaaaabde17e38
Shamilov007 <atambek.tiger12@gmail.com>
2026-05-12 21:49:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,37 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, F
+import os
+from threading import Thread
+from flask import Flask
+
+from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.filters import Command
 from aiogram.types import Message
+
 from config import BOT_TOKEN
 from handlers import all_routers
 from keyboards import reply_menu_kb
 
 logging.basicConfig(level=logging.INFO)
+
+# ---------------- FLASK ----------------
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run_web():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+def keep_alive():
+    t = Thread(target=run_web)
+    t.start()
+
+# ---------------- BOT ----------------
 
 async def cmd_cancel(message: Message):
     await message.answer(
@@ -23,6 +45,7 @@ async def main():
         token=BOT_TOKEN,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
+
     dp = Dispatcher()
 
     dp.message.register(cmd_cancel, Command("cancel"))
@@ -31,7 +54,9 @@ async def main():
         dp.include_router(router)
 
     logging.info("Bot started!")
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    keep_alive()
     asyncio.run(main())
